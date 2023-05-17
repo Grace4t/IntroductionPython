025aef9b77a4d10cf0c0138e9b75495489847dcb
Graciela Torres <grace4torres@gmail.com>
2023-05-17 11:22:34 -0600

    Imprimiento en 2s

---
+++ nested_loops/mario.py
@@ -0,0 +1,8 @@
+#para imprimir un arreglo de 2d
+print("#")
+print("#")
+print("#")
+
+#Dinamicamente
+for _ in range(3):
+    print("#")
---
+++ nested_loops/mario1.py
@@ -0,0 +1,8 @@
+#funtion, allowws to create simplification about complicate ideas
+def main():
+    print_colum(3)
+
+def print_colum(height):
+    for _ in range(height):
+        print("#")
+main()
