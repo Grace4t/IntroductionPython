d647fd4eeaa243915b47f06a5074d492b9a80a00
Graciela Torres <grace4torres@gmail.com>
2023-05-22 12:56:35 -0600

    otra forma de imprimir el cubo

---
+++ nested_loops/mario6.py
@@ -0,0 +1,10 @@
+def main():
+    print_square(5)
+
+def print_square(size):
+    # For para cada fila (row)
+    for i in range(size):
+        print_row(size)
+def print_row(width):
+    print("#" * width)
+main()
