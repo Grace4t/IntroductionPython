9f57f2069bf1a200895794efa08a07e3e5094892
Graciela Torres <grace4torres@gmail.com>
2023-06-03 11:47:47 -0600

    Agregue carpeta exceptions

--- nested_loops/mario7.py
+++ nested_loops/mario7.py
@@ -4,5 +4,9 @@ def main():
 def print_square(size):
     # For para cada fila (row)
     for i in range(size):
-        print("#" * size)
+        print_row(size)
+
+def print_row(width):
+    print("#" * width)
+    
 main()
